f0851a69f57b54e9134fcb5f429e7bd9065392e6
alone-wolf <1031629044@qq.com>
2020-05-09 16:04:18 +0800

    backup
    Signed-off-by: alone-wolf <1031629044@qq.com>

--- app/src/main/java/com/wh/mydeskclock/Panel/PanelMoreFragment.java
+++ app/src/main/java/com/wh/mydeskclock/Panel/PanelMoreFragment.java
@@ -50,11 +50,11 @@ public class PanelMoreFragment extends Fragment {
     }
 
     private View addCheckBox(Context context, String TITLE, String KEY) {
-        View view = LayoutInflater.from(context).inflate(R.layout.item_checkbox_fragment_panel_more, null);
-        TextView textView = view.findViewById(R.id.tv_item_checkbox_fragment_panel_more);
+        View view = LayoutInflater.from(context).inflate(R.layout.item_checkbox_preference, null);
+        TextView textView = view.findViewById(R.id.tv_title_item_checkbox_preference);
         textView.setText(TITLE);
 
-        CheckBox checkBox = view.findViewById(R.id.cb_item_checkbox_fragment_panel_more);
+        CheckBox checkBox = view.findViewById(R.id.cb_item_checkbox_preference);
         checkBox.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
             @Override
             public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
---
+++ app/src/main/java/com/wh/mydeskclock/Panel/Player/PanelPlayerFragment.java
@@ -0,0 +1,29 @@
+package com.wh.mydeskclock.Panel.Player;
+
+import android.os.Bundle;
+
+import androidx.fragment.app.Fragment;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.wh.mydeskclock.R;
+
+/**
+ * A simple {@link Fragment} subclass.
+ */
+public class PanelPlayerFragment extends Fragment {
+
+    public PanelPlayerFragment() {
+        // Required empty public constructor
+    }
+
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_player, container, false);
+    }
+}

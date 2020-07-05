a2ddc222e67873764aba9c9a3c8fea7709ee6efc
alone-wolf <1031629044@qq.com>
2020-07-05 16:13:49 +0800

    重构 全屏
    Signed-off-by: alone-wolf <1031629044@qq.com>

--- app/src/main/java/com/wh/mydeskclock/MainActivity.java
+++ app/src/main/java/com/wh/mydeskclock/MainActivity.java
@@ -1,65 +1,19 @@
 package com.wh.mydeskclock;
 
-import androidx.appcompat.app.ActionBar;
 import androidx.appcompat.app.AppCompatActivity;
 
-
 import android.os.Bundle;
-import android.util.Log;
-import android.view.WindowManager;
-import android.widget.FrameLayout;
-import android.widget.Toast;
+import android.view.View;
 
-import org.jsoup.Jsoup;
-import org.jsoup.nodes.Document;
-
-
-import java.io.IOException;
-
+import okhttp3.internal.Util;
 
 public class MainActivity extends AppCompatActivity {
-    private String TAG = "WH_MainActivity";
-    private FrameLayout frameLayout_con;
-
-    @Override
-    public void onWindowFocusChanged(boolean hasFocus) {
-        super.onWindowFocusChanged(hasFocus);
-        Utils.setWindowSystemUiVisibility(this);
-    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        if (!Utils.Check(this, Utils.NONE_TEST)) {
-            Toast.makeText(MainActivity.this, "证书已过期，请到酷安更新", Toast.LENGTH_LONG).show();
-            new Thread("quit") {
-                @Override
-                public void run() {
-                    super.run();
-                    try {
-                        sleep(3000);
-                        finish();
-                    } catch (InterruptedException e) {
-                        e.printStackTrace();
-                    }
-                }
-            }.start();
-        }
-
-        ActionBar actionBar = getSupportActionBar();
-        if (actionBar != null) {
-            actionBar.hide();
-        }
-
-        getWindow().addFlags(WindowManager.LayoutParams.FLAG_KEEP_SCREEN_ON);
+        UIUtils.setFullScreen(getWindow()).start();
     }
-
-    @Override
-    protected void onDestroy() {
-        super.onDestroy();
-        getWindow().clearFlags(WindowManager.LayoutParams.FLAG_KEEP_SCREEN_ON);
-    }
-
 }

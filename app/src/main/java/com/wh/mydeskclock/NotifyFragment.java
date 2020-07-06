416fe1ddc02e7b7d970185c3f2b89a9d09dee35c
alone-wolf <1031629044@qq.com>
2020-07-06 19:07:23 +0800

    notify：增加点击x号隐藏notify notify：增加显示notify接收时间
    Signed-off-by: alone-wolf <1031629044@qq.com>

--- app/src/main/java/com/wh/mydeskclock/NotifyFragment.java
+++ app/src/main/java/com/wh/mydeskclock/NotifyFragment.java
@@ -1,5 +1,6 @@
 package com.wh.mydeskclock;
 
+import android.annotation.SuppressLint;
 import android.content.BroadcastReceiver;
 import android.content.Context;
 import android.content.Intent;
@@ -15,8 +16,13 @@ import android.os.Message;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageView;
 import android.widget.LinearLayout;
 import android.widget.TextView;
+
+import com.wh.mydeskclock.Utils.TimeUtils;
+
+import org.jetbrains.annotations.NotNull;
 
 public class NotifyFragment extends Fragment {
     String TAG = "WH_"+NotifyFragment.class.getSimpleName();
@@ -29,6 +35,7 @@ public class NotifyFragment extends Fragment {
     private TextView tv_title;
     private TextView tv_create_time;
     private View rootView;
+    private ImageView iv_close;
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
@@ -52,8 +59,9 @@ public class NotifyFragment extends Fragment {
         requireContext().registerReceiver(notifyReceiver,intentFilter);
     }
 
+    @SuppressLint("InflateParams")
     @Override
-    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+    public View onCreateView(@NotNull LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
 
         rootView = LayoutInflater.from(requireContext()).inflate(R.layout.item_notify,null,false);
@@ -62,6 +70,14 @@ public class NotifyFragment extends Fragment {
         tv_title = rootView.findViewById(R.id.tv_notify_item_title);
         tv_create_time = rootView.findViewById(R.id.tv_notify_item_create_time);
         rootView.setVisibility(View.GONE);
+
+        iv_close = rootView.findViewById(R.id.iv_close);
+        iv_close.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                rootView.setVisibility(View.GONE);
+            }
+        });
 
         return rootView;
     }
@@ -96,6 +112,7 @@ public class NotifyFragment extends Fragment {
                     tv_con.setText(NOTIFY);
                     tv_device.setText(DEVICE);
                     tv_title.setText(TITLE);
+                    tv_create_time.setText(TimeUtils.getFormattedTime(System.currentTimeMillis()));
                     break;
                 }
             }

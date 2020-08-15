f815f7da50d5b4f070b59423d7c7fd6fd0b955f9
alone-wolf <1031629044@qq.com>
2020-08-15 20:19:50 +0800

    web：将备胎转正，准备抛弃原页面 web：新页面在请求的header中加入了access_token，用于校验登录

--- app/src/main/assets/web/static/index.js
+++ app/src/main/assets/web/static/index.js
@@ -6,138 +6,297 @@ let toBool = function(bool_str) {
     }
 }
 
+let ensure2num = function(num) {
+    return num < 10 ? ('0' + num) : num;
+}
+
 let init = function() {
-    if (localStorage.getItem('inited') != 1) {
-        localStorage.setItem('inited', 1);
+    if (localStorage.getItem('api_list_show') == null) {
         localStorage.setItem('api_list_show', false);
+    }
+    if (localStorage.getItem('task_list_done_show') == null) {
         localStorage.setItem('task_list_done_show', false);
+    }
+    if (localStorage.getItem('task_default_title') == null) {
         localStorage.setItem('task_default_title', 'default title');
+    }
+    if (localStorage.getItem('device_name') == null) {
         localStorage.setItem('device_name', 'default device');
+    }
+    if (localStorage.getItem('task_list_get_undone_only') == null) {
         localStorage.setItem('task_list_get_undone_only', true);
+    }
+    if (localStorage.getItem('judge_item_con_is_url') == null) {
+        localStorage.setItem('judge_item_con_is_url', true);
+    }
+    if (localStorage.getItem('access_token') == null) {
+        localStorage.setItem('access_token', "asdfghjkl");
     }
 }
 
+let judge_url = function(strstr) {
+    var part = /^(https?:\/\/)?(www\.)?[-a-zA-Z0-9@:%._\+~#=]{1,256}\.[a-zA-Z0-9()]{1,6}\b([-a-zA-Z0-9()!@:%_\+.~#?&\/\/=]*)/;
+    return part.test(strstr);
+}
+
+
+//     // 本日 本月 本年
+// let timeData_ms = Date.now();
+// let datedate = new Date();
+// let timeDate_year = datedate.getFullYear();
+// let timeDate_month = datedate.getMonth() + 1;
+// let timeDate_date = datedate.getDate();
+
+// let getFirstMS_year = function() {
+//     return timeDate_year + "-00-00 00:00:000";
+// }
+
+// let getFirstMS_month = function() {
+//     return timeDate_year + "-" + ensure2num(timeDate_month) + "-00 00:00:000";
+// }
+
+// let getFirstMS_date = function() {
+//     return timeDate_year + "-" + ensure2num(timeDate_month) + "-" + ensure2num(timeDate_date) + " 00:00:000";
+// }
+
+// console.log(getFirstMS_year() + " " + getFirstMS_month() + " " + getFirstMS_date());
+// console.log(getMSFromTimeStamp(getFirstMS_year()));
+
+let FormatDateTime = function(UnixTime) {
+    var date = new Date(parseInt(UnixTime));
+    var y = date.getFullYear();
+    var m = date.getMonth() + 1;
+    m = m < 10 ? ('0' + m) : m;
+    var d = date.getDate();
+    d = d < 10 ? ('0' + d) : d;
+    var h = date.getHours();
+    h = h < 10 ? ('0' + h) : h;
+    var minute = date.getMinutes();
+    var second = date.getSeconds();
+    minute = minute < 10 ? ('0' + minute) : minute;
+    second = second < 10 ? ('0' + second) : second;
+    return y + '-' + m + '-' + d + ' ' + h + ':' + minute + ':' + second;
+}
+
 init();
-
 new Vue({
-    el: '#panel_task',
+    el: "#page_main",
     data: {
-        task_title: localStorage.getItem('task_default_title'),
-        task: '',
-        device: localStorage.getItem('device_name'),
-
-        task_con_for_show: 'blank',
-        task_list_get_undone_only: toBool(localStorage.getItem('task_list_get_undone_only')),
-
-        task_list_done_show: toBool(localStorage.getItem('task_list_done_show')),
-        tasks: [],
-
-        api_list_show: toBool(localStorage.getItem('api_list_show')),
-        apis: []
+        apps: {
+            task: {
+                active: "active",
+                showed: true,
+                task_title: localStorage.getItem('task_default_title'),
+                tasks: [],
+                task: '',
+                task_item: {
+                    id: -1,
+                    title: "blank title",
+                    con: "blank con",
+                    device: "blank device",
+                    createTime: "0000-00-00 00:00:00",
+                    readDone: false
+                },
+                task_list_get_undone_only: toBool(localStorage.getItem('task_list_get_undone_only')),
+                task_list_done_show: toBool(localStorage.getItem('task_list_done_show')),
+            },
+            notify: {
+                active: "",
+                showed: false
+            },
+            sticky: {
+                active: "",
+                showed: false,
+                stickies: []
+            },
+            remote_ctrl: {
+                active: "",
+                showed: false
+            },
+            settings: {
+                active: "",
+                showed: false,
+                device: localStorage.getItem('device_name'),
+                api_list_show: toBool(localStorage.getItem('api_list_show')),
+                apis: [],
+                judge_url: toBool(localStorage.getItem('judge_item_con_is_url')),
+                access_token: localStorage.getItem('access_token'),
+            }
+        },
     },
     methods: {
-        send: function() {
-            if (this.task == "") {
+        task_send: function() {
+            if (this.apps.task.task == "") {
                 alert("task can't be blank");
                 return;
             }
             var that = this;
-            var task_tmp = this.task;
-            this.task = "";
+            var task_tmp = this.apps.task.task;
+            this.apps.task.task = "";
             axios.get("/task/add", {
                 params: {
-                    "title": this.task_title,
+                    "title": this.apps.task.task_title,
                     "task": task_tmp,
-                    "device": this.device
-                }
+                    "device": this.apps.settings.device
+                },
+                headers: { access_token: this.settings.access_token }
             }).then(function(response) {
                 alert("ok");
-                that.fetch();
+                that.task_fetch();
             }).catch(function(error) {
                 alert(error);
             });
         },
-        fetch: function() {
+        task_fetch: function() {
             var address = '/task/get/';
-            if (this.task_list_get_undone_only) {
+            if (this.apps.task.task_list_get_undone_only) {
                 address += 'undone';
             } else {
                 address += 'all';
             }
             that = this
-            axios.get(address)
+            axios.get(address, {
+                    headers: { access_token: localStorage.getItem('access_token') }
+                })
                 .then(function(response) {
-                    that.tasks = response.data.data;
+                    that.apps.task.tasks = response.data.data;
                 });
         },
-        set_done: function(id) {
+        task_set_done: function(id) {
             var that = this;
-            axios.get("/task/done/" + id)
+            axios.get("/task/done/" + id, {
+                    headers: { access_token: localStorage.getItem('access_token') }
+                })
                 .then(function(response) {
-                    that.fetch();
+                    that.task_fetch();
                 });
         },
-        set_undone: function(id) {
+        task_set_undone: function(id) {
             var that = this;
-            axios.get("/task/undone/" + id)
+            axios.get("/task/undone/" + id, {
+                    headers: { access_token: localStorage.getItem('access_token') }
+                })
                 .then(function(response) {
-                    that.fetch();
+                    that.task_fetch();
                 });
         },
-        FormatDateTime: function(UnixTime) {
-            var date = new Date(parseInt(UnixTime));
-            var y = date.getFullYear();
-            var m = date.getMonth() + 1;
-            m = m < 10 ? ('0' + m) : m;
-            var d = date.getDate();
-            d = d < 10 ? ('0' + d) : d;
-            var h = date.getHours();
-            h = h < 10 ? ('0' + h) : h;
-            var minute = date.getMinutes();
-            var second = date.getSeconds();
-            minute = minute < 10 ? ('0' + minute) : minute;
-            second = second < 10 ? ('0' + second) : second;
-            return y + '-' + m + '-' + d + ' ' + h + ':' + minute + ':' + second;
+        task_show_detail: function(id) {
+            this.apps.task.tasks.forEach(element => {
+                if (element.id == id) {
+                    this.apps.task.task_item.id = id;
+                    this.apps.task.task_item.title = element.title;
+                    this.apps.task.task_item.con = element.con;
+                    this.apps.task.task_item.device = element.deviceName;
+                    this.apps.task.task_item.createTime = element.createTime;
+                    this.apps.task.task_item.readDone = element.readDone;
+                    $("#task_detail").modal();
+                }
+                return;
+            });
         },
-        save: function() {
+        task_reset_title: function() {
+            this.apps.task.task_title = localStorage.getItem("task_default_title");
+        },
+        sticky_fetch: function() {
+            var that = this;
+            axios.get('/sticky/get/all', {
+                headers: { access_token: localStorage.getItem('access_token') }
+            }).then(function(response) {
+                that.apps.sticky.stickies = response.data.data;
+            });
+        },
+        ui_format_time: function(timeMS) {
+            return FormatDateTime(timeMS);
+        },
+        settings_save: function(show) {
             // 保存api list 的配置
-            localStorage.setItem('api_list_show', this.api_list_show);
+            localStorage.setItem('api_list_show', this.apps.settings.api_list_show);
             // 保存task list 的配置
-            localStorage.setItem('task_list_done_show', this.task_list_done_show);
+            localStorage.setItem('task_list_done_show', this.apps.task.task_list_done_show);
             // 保存 default done task title
-            localStorage.setItem('task_default_title', this.task_title);
+            localStorage.setItem('task_default_title', this.apps.task.task_title);
             // 保存 device name
-            localStorage.setItem('device_name', this.device);
+            localStorage.setItem('device_name', this.apps.settings.device);
             // 保存 是否只获取un done items
-            localStorage.setItem('task_list_get_undone_only', this.task_list_get_undone_only);
+            localStorage.setItem('task_list_get_undone_only', this.apps.task.task_list_get_undone_only);
+
+            localStorage.setItem('judge_item_con_is_url', this.apps.settings.judge_url);
+
+            localStorage.setItem('access_token', this.settings.access_token);
+            alert("save done");
         },
-        openSetting: function() {
-            $("#settingDialogPanel").modal();
+        settings_api_fetch: function() {
+            var that = this;
+            axios.get("/api/get", {
+                headers: { access_token: localStorage.getItem('access_token') }
+            }).then(function(response) {
+                that.apps.settings.apis = response.data.data;
+            });
         },
-        show_task_detail: function(con) {
-            this.task_con_for_show = con;
-            $("#task_detail").modal();
-        },
-        setUnInit: function() {
-            localStorage.setItem('inited', 0);
-            alert('Set UnInit Done\nPage Will ReFresh');
+        settings_re_init: function() {
+            window.localStorage.clear();
+            alert('Set ReInit\nPage Will ReFresh');
             location.reload();
         },
-        resetTitle: function() {
-            this.task_title = localStorage.getItem("task_default_title");
-        },
-        help: function() {
+        settings_help: function() {
             var a = "1.提交和获取task条目过程使用的是纯正的ajax技术，因此可以实现无刷新更新数据\n";
             var b = "2.点击Send Task中 ‘title’ 文字，可以重置输入的值\n";
             var c = "3.类比上一条，点击task相应位置，清空task输入框\n";
             var d = "4.为了提高响应速度，减少资源浪费，建议开启 Only Get UnDone Task Items 设置项目\n";
             alert(a + b + c + d);
+        },
+        switchPanel: function(panelName) {
+            if (panelName == "sticky") {
+                if (this.apps.sticky.active == "active") {
+                    $("#sticky_send").modal();
+                }
+            }
+
+            var active_status = {
+                task: "",
+                notify: "",
+                sticky: "",
+                remote_ctrl: "",
+                settings: ""
+            }
+            active_status[panelName] = "active";
+            Object.keys(active_status).forEach(element => {
+                this.apps[element].active = active_status[element];
+            });
+            this.apps[panelName].showed = true;
+        },
+        utils_judge_url: function(sss) {
+            return judge_url(sss);
+        },
+        util_open_new_window: function(urll) {
+            window.open(urll);
         }
     },
-    created() {
-        this.fetch();
-        axios.get("/static/api_list.json").then(function(response) {
-            that.apis = response.data;
-        });
+    watch: {
+        "apps.notify.showed": function(newVal, oldVal) {
+            // 更新数据
+            if (newVal) {
+                console.log(newVal);
+            }
+        },
+        "apps.sticky.showed": function(newVal) {
+            if (newVal) {
+                this.sticky_fetch();
+            }
+        },
+        "apps.remote_ctrl.showed": function(newVal) {
+
+        },
+        "apps.settings.api_list_show": function(newVal) {
+            if (newVal && this.apps.settings.apis.length == 0) {
+                this.settings_api_fetch();
+            }
+        }
+    },
+    mounted() {
+        this.task_fetch();
+        if (this.apps.settings.api_list_show) {
+            this.settings_api_fetch();
+        }
     }
-});
+})
--- app/src/main/assets/web/static/index1.js
+++ app/src/main/assets/web/static/index1.js
@@ -6,145 +6,60 @@ let toBool = function(bool_str) {
     }
 }
 
-let ensure2num = function(num) {
-    return num < 10 ? ('0' + num) : num;
-}
-
 let init = function() {
-    if (localStorage.getItem('api_list_show') == null) {
+    if (localStorage.getItem('inited') != 1) {
+        localStorage.setItem('inited', 1);
         localStorage.setItem('api_list_show', false);
-    }
-    if (localStorage.getItem('task_list_done_show') == null) {
         localStorage.setItem('task_list_done_show', false);
-    }
-    if (localStorage.getItem('task_default_title') == null) {
         localStorage.setItem('task_default_title', 'default title');
-    }
-    if (localStorage.getItem('device_name') == null) {
         localStorage.setItem('device_name', 'default device');
-    }
-    if (localStorage.getItem('task_list_get_undone_only') == null) {
         localStorage.setItem('task_list_get_undone_only', true);
     }
-    if (localStorage.getItem('judge_item_con_is_url') == null) {
-        localStorage.setItem('judge_item_con_is_url', true);
-    }
-}
-
-let judge_url = function(strstr) {
-    var part = /^(https?:\/\/)?(www\.)?[-a-zA-Z0-9@:%._\+~#=]{1,256}\.[a-zA-Z0-9()]{1,6}\b([-a-zA-Z0-9()!@:%_\+.~#?&\/\/=]*)/;
-    return part.test(strstr);
-}
-
-
-//     // 本日 本月 本年
-// let timeData_ms = Date.now();
-// let datedate = new Date();
-// let timeDate_year = datedate.getFullYear();
-// let timeDate_month = datedate.getMonth() + 1;
-// let timeDate_date = datedate.getDate();
-
-// let getFirstMS_year = function() {
-//     return timeDate_year + "-00-00 00:00:000";
-// }
-
-// let getFirstMS_month = function() {
-//     return timeDate_year + "-" + ensure2num(timeDate_month) + "-00 00:00:000";
-// }
-
-// let getFirstMS_date = function() {
-//     return timeDate_year + "-" + ensure2num(timeDate_month) + "-" + ensure2num(timeDate_date) + " 00:00:000";
-// }
-
-// console.log(getFirstMS_year() + " " + getFirstMS_month() + " " + getFirstMS_date());
-// console.log(getMSFromTimeStamp(getFirstMS_year()));
-
-let FormatDateTime = function(UnixTime) {
-    var date = new Date(parseInt(UnixTime));
-    var y = date.getFullYear();
-    var m = date.getMonth() + 1;
-    m = m < 10 ? ('0' + m) : m;
-    var d = date.getDate();
-    d = d < 10 ? ('0' + d) : d;
-    var h = date.getHours();
-    h = h < 10 ? ('0' + h) : h;
-    var minute = date.getMinutes();
-    var second = date.getSeconds();
-    minute = minute < 10 ? ('0' + minute) : minute;
-    second = second < 10 ? ('0' + second) : second;
-    return y + '-' + m + '-' + d + ' ' + h + ':' + minute + ':' + second;
 }
 
 init();
+
 new Vue({
-    el: "#page_main",
+    el: '#panel_task',
     data: {
-        apps: {
-            task: {
-                active: "active",
-                showed: true,
-                task_title: localStorage.getItem('task_default_title'),
-                tasks: [],
-                task: '',
-                task_item: {
-                    id: -1,
-                    title: "blank title",
-                    con: "blank con",
-                    device: "blank device",
-                    createTime: "0000-00-00 00:00:00",
-                    readDone: false
-                },
-                task_list_get_undone_only: toBool(localStorage.getItem('task_list_get_undone_only')),
-                task_list_done_show: toBool(localStorage.getItem('task_list_done_show')),
-            },
-            notify: {
-                active: "",
-                showed: false
-            },
-            sticky: {
-                active: "",
-                showed: false,
-                stickies: []
-            },
-            remote_ctrl: {
-                active: "",
-                showed: false
-            },
-            settings: {
-                active: "",
-                showed: false,
-                device: localStorage.getItem('device_name'),
-                api_list_show: toBool(localStorage.getItem('api_list_show')),
-                apis: [],
-                judge_url: toBool(localStorage.getItem('judge_item_con_is_url'))
-            }
-        },
+        task_title: localStorage.getItem('task_default_title'),
+        task: '',
+        device: localStorage.getItem('device_name'),
+
+        task_con_for_show: 'blank',
+        task_list_get_undone_only: toBool(localStorage.getItem('task_list_get_undone_only')),
+
+        task_list_done_show: toBool(localStorage.getItem('task_list_done_show')),
+        tasks: [],
+
+        api_list_show: toBool(localStorage.getItem('api_list_show')),
+        apis: []
     },
     methods: {
-        task_send: function() {
-            if (this.apps.task.task == "") {
+        send: function() {
+            if (this.task == "") {
                 alert("task can't be blank");
                 return;
             }
             var that = this;
-            var task_tmp = this.apps.task.task;
-            this.apps.task.task = "";
+            var task_tmp = this.task;
+            this.task = "";
             axios.get("/task/add", {
                 params: {
-                    "title": this.apps.task.task_title,
+                    "title": this.task_title,
                     "task": task_tmp,
-                    "device": this.apps.settings.device
+                    "device": this.device
                 }
             }).then(function(response) {
                 alert("ok");
-                that.task_fetch();
+                that.fetch();
             }).catch(function(error) {
                 alert(error);
             });
         },
-        task_fetch: function() {
+        fetch: function() {
             var address = '/task/get/';
-            if (this.apps.task.task_list_get_undone_only) {
+            if (this.task_list_get_undone_only) {
                 address += 'undone';
             } else {
                 address += 'all';
@@ -152,134 +67,77 @@ new Vue({
             that = this
             axios.get(address)
                 .then(function(response) {
-                    that.apps.task.tasks = response.data.data;
+                    that.tasks = response.data.data;
                 });
         },
-        task_set_done: function(id) {
+        set_done: function(id) {
             var that = this;
             axios.get("/task/done/" + id)
                 .then(function(response) {
-                    that.task_fetch();
+                    that.fetch();
                 });
         },
-        task_set_undone: function(id) {
+        set_undone: function(id) {
             var that = this;
             axios.get("/task/undone/" + id)
                 .then(function(response) {
-                    that.task_fetch();
+                    that.fetch();
                 });
         },
-        task_show_detail: function(id) {
-            this.apps.task.tasks.forEach(element => {
-                if (element.id == id) {
-                    this.apps.task.task_item.id = id;
-                    this.apps.task.task_item.title = element.title;
-                    this.apps.task.task_item.con = element.con;
-                    this.apps.task.task_item.device = element.deviceName;
-                    this.apps.task.task_item.createTime = element.createTime;
-                    this.apps.task.task_item.readDone = element.readDone;
-                    $("#task_detail").modal();
-                }
-                return;
-            });
+        FormatDateTime: function(UnixTime) {
+            var date = new Date(parseInt(UnixTime));
+            var y = date.getFullYear();
+            var m = date.getMonth() + 1;
+            m = m < 10 ? ('0' + m) : m;
+            var d = date.getDate();
+            d = d < 10 ? ('0' + d) : d;
+            var h = date.getHours();
+            h = h < 10 ? ('0' + h) : h;
+            var minute = date.getMinutes();
+            var second = date.getSeconds();
+            minute = minute < 10 ? ('0' + minute) : minute;
+            second = second < 10 ? ('0' + second) : second;
+            return y + '-' + m + '-' + d + ' ' + h + ':' + minute + ':' + second;
         },
-        task_reset_title: function() {
-            this.apps.task.task_title = localStorage.getItem("task_default_title");
-        },
-        sticky_fetch: function() {
-            var that = this;
-            axios.get('/sticky/get/all').then(function(response) {
-                that.apps.sticky.stickies = response.data.data;
-            });
-        },
-        ui_format_time: function(timeMS) {
-            return FormatDateTime(timeMS);
-        },
-        settings_save: function(show) {
+        save: function() {
             // 保存api list 的配置
-            localStorage.setItem('api_list_show', this.apps.settings.api_list_show);
+            localStorage.setItem('api_list_show', this.api_list_show);
             // 保存task list 的配置
-            localStorage.setItem('task_list_done_show', this.apps.task.task_list_done_show);
+            localStorage.setItem('task_list_done_show', this.task_list_done_show);
             // 保存 default done task title
-            localStorage.setItem('task_default_title', this.apps.task.task_title);
+            localStorage.setItem('task_default_title', this.task_title);
             // 保存 device name
-            localStorage.setItem('device_name', this.apps.settings.device);
+            localStorage.setItem('device_name', this.device);
             // 保存 是否只获取un done items
-            localStorage.setItem('task_list_get_undone_only', this.apps.task.task_list_get_undone_only);
-
-            localStorage.setItem('judge_item_con_is_url', this.apps.settings.judge_url);
-            alert("save done");
+            localStorage.setItem('task_list_get_undone_only', this.task_list_get_undone_only);
         },
-        settings_api_fetch: function() {
-            var that = this;
-            axios.get("/api/get").then(function(response) {
-                that.apps.settings.apis = response.data.data;
-            });
+        openSetting: function() {
+            $("#settingDialogPanel").modal();
         },
-        settings_re_init: function() {
-            window.localStorage.clear();
-            alert('Set ReInit\nPage Will ReFresh');
+        show_task_detail: function(con) {
+            this.task_con_for_show = con;
+            $("#task_detail").modal();
+        },
+        setUnInit: function() {
+            localStorage.setItem('inited', 0);
+            alert('Set UnInit Done\nPage Will ReFresh');
             location.reload();
         },
-        settings_help: function() {
+        resetTitle: function() {
+            this.task_title = localStorage.getItem("task_default_title");
+        },
+        help: function() {
             var a = "1.提交和获取task条目过程使用的是纯正的ajax技术，因此可以实现无刷新更新数据\n";
             var b = "2.点击Send Task中 ‘title’ 文字，可以重置输入的值\n";
             var c = "3.类比上一条，点击task相应位置，清空task输入框\n";
             var d = "4.为了提高响应速度，减少资源浪费，建议开启 Only Get UnDone Task Items 设置项目\n";
             alert(a + b + c + d);
-        },
-        switchPanel: function(panelName) {
-            if (panelName == "sticky") {
-                if (this.apps.sticky.active == "active") {
-                    $("#sticky_send").modal();
-                }
-            }
-
-            var active_status = {
-                task: "",
-                notify: "",
-                sticky: "",
-                remote_ctrl: "",
-                settings: ""
-            }
-            active_status[panelName] = "active";
-            Object.keys(active_status).forEach(element => {
-                this.apps[element].active = active_status[element];
-            });
-            this.apps[panelName].showed = true;
-        },
-        utils_judge_url: function(sss) {
-            return judge_url(sss);
-        },
-        util_open_new_window: function(urll) {
-            window.open(urll);
         }
     },
-    watch: {
-        "apps.notify.showed": function(newVal, oldVal) {
-            // 更新数据
-            if (newVal) {
-                console.log(newVal);
-            }
-        },
-        "apps.sticky.showed": function(newVal) {
-            if (newVal) {
-                this.sticky_fetch();
-            }
-        },
-        "apps.remote_ctrl.showed": function(newVal) {
-
-        },
-        "apps.settings.api_list_show": function(newVal) {
-            if (newVal && this.apps.settings.apis.length == 0) {
-                this.settings_api_fetch();
-            }
-        }
-    },
-    mounted() {
-        this.task_fetch();
-        if (this.apps.settings.api_list_show) {
-            this.settings_api_fetch();
-        }
+    created() {
+        this.fetch();
+        axios.get("/static/api_list.json").then(function(response) {
+            that.apis = response.data;
+        });
     }
-})
+});

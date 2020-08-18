05e6e65c7a79c4962ca6815e8f4be1f88b6ae730
alone-wolf <1031629044@qq.com>
2020-08-18 16:51:55 +0800

    增加了一些文件类型

--- app/src/main/java/com/wh/mydeskclock/utils/FileUtils.java
+++ app/src/main/java/com/wh/mydeskclock/utils/FileUtils.java
@@ -3,9 +3,13 @@ package com.wh.mydeskclock.utils;
 public class FileUtils {
     public static class MimeType{
         public static class APPLICATION{
+            public static String HEAD = "application";
             public static String ALL = "application/*";
             public static String OCTET_STREAM = "application/octet-stream";
             public static String APK = "application/vnd.android.package-archive";
+            public static String ZIP = HEAD+"/zip";
+            public static String TAR = HEAD+"/x-tar";
+            public static String SH = HEAD+"/x-sh";
         }
         public static class TEXT{
             public static String ALL = "text/*";
